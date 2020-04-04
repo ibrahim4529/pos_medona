4c5973f61715b32af7801d799b63c4a1148ad713
Ibrahim Hanif <ibrahim4529@github.com>
2020-04-04 21:58:27 +0700

    Add laravel/ui

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginRequest;
+use App\User;
+
+class AuthController extends Controller
+{
+   /**
+    * Login a function for making authentication for app
+    * @param LoginRequest $request
+    * @return Response
+    */
+
+    public function login(LoginRequest $request)
+    {
+
+    }
+}
--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,21 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    
+    
+    /**
+    * A function for make custom error message validation
+    * Error Format
+    * errors: [
+    *    {
+    *        "message": "error message",
+    *        "field": "name of field"
+    *    }
+    * ]
+    * @param Validator 
+    */
+    
+    public function errorMessage(Validator $validator){
+        foreach($validator->message as )
+    }
 }
